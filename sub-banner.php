e999fadb451c4da2c6d1f88bc3d43140c7164adf
Malena Andrade <malena.andrade@gmail.com>
2016-06-08 16:25:31 -0400

    Add timeline include, styling

---
+++ sub-banner.php
@@ -0,0 +1,26 @@
+<div class="container">
+	<div class="timeline">
+		<div class="timeline-progress">
+			<div class="timeline-progress-completion" style="width: 14.5%;"></div>
+			<div class="timeline-progress-percentage timeline-active" style="margin-left: 13%;">
+				<div class="timeline-chevron"></div>
+				<div class="timeline-phase">Creative Strategy<span>JUNE</span></div>
+			</div>
+			<div class="timeline-progress-percentage" style="margin-left: 38%;">
+				<div class="timeline-chevron"></div>
+				<div class="timeline-phase">Concepting<span>JULY</span></div>
+			</div>
+			<div class="timeline-progress-percentage" style="margin-left: 62%;">
+				<div class="timeline-chevron"></div>
+				<div class="timeline-phase">Refinement<span>SEPTEMBER</span></div>
+			</div>
+			<div class="timeline-progress-percentage" style="margin-left: 87%;">
+				<div class="timeline-chevron"></div>
+				<div class="timeline-phase">Guidance<span>SEPTEMBER</span></div>
+			</div>
+		</div>
+	</div>
+	<div class="description">
+		<p><strong>We're updating our brand identity using Mozilla open source principles, and everyone's invited.</strong> In this, our Creative Strategy phase, we welcome your ideas for directions Mozilla might explore to present itself anew to the world. Comment or post a link to an example of open design done well. On June 15, we'll share the directions we've thought of thus far. <a href="/about">Learn more</a>.</p>
+	</div>
+</div>
